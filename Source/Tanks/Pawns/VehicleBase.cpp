f3ae1d9fde170655c83ddee4b825c715b36950ce
DivadTenni <davidctenni@gmail.com>
2021-07-07 00:12:06 +1000

    Added: Declared new components in Vehicle Base Class

--- Source/Tanks/Pawns/VehicleBase.cpp
+++ Source/Tanks/Pawns/VehicleBase.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
+#include "Tanks/Pawns/VehicleBase.h"
 
-
-#include "Pawns/VehicleBase.h"
 
 // Sets default values
 AVehicleBase::AVehicleBase()
@@ -9,6 +8,16 @@ AVehicleBase::AVehicleBase()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMesh->SetupAttachment(RootComponent);
+	
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Turret Mesh"));
+	TurretMesh->SetupAttachment(BaseMesh);
+	
+	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn Point"));
+	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
 }
 
 // Called when the game starts or when spawned
--- Source/Tanks/Pawns/VehicleBase.h
+++ Source/Tanks/Pawns/VehicleBase.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "Components/CapsuleComponent.h"
+
 #include "VehicleBase.generated.h"
 
 UCLASS()
@@ -11,19 +13,28 @@ class TANKS_API AVehicleBase : public APawn
 {
 	GENERATED_BODY()
 
+private:
+	UPROPERTY()
+	UCapsuleComponent* CapsuleComponent = nullptr;
+
+	UPROPERTY()
+	UStaticMeshComponent* BaseMesh = nullptr;
+	
+	UPROPERTY()
+	UStaticMeshComponent* TurretMesh = nullptr;
+	
+	UPROPERTY()
+	USceneComponent* ProjectileSpawnPoint = nullptr;
 public:
 	// Sets default values for this pawn's properties
 	AVehicleBase();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
