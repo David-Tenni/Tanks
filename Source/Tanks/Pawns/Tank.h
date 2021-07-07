2c67b7ab1c84aca10116a2d6f9f50d3b18b0acd2
DivadTenni <davidctenni@gmail.com>
2021-07-07 18:45:42 +1000

    Added: Tank class as a child of Vehicle

---
+++ Source/Tanks/Pawns/Tank.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VehicleBase.h"
+#include "Tank.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKS_API ATank : public AVehicleBase
+{
+	GENERATED_BODY()
+	
+};
