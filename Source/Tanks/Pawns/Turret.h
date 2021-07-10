0b08fab6ff3faa3c23177d401cf5a2f3b33ad94f
DivadTenni <davidctenni@gmail.com>
2021-07-10 20:00:43 +1000

    Added: Turret Class

---
+++ Source/Tanks/Pawns/Turret.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "VehicleBase.h"
+#include "Turret.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKS_API ATurret : public AVehicleBase
+{
+	GENERATED_BODY()
+	
+};
