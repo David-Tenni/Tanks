de20871e7e5942191d451a96b72fa0f45631bc19
DivadTenni <davidctenni@gmail.com>
2021-07-11 15:24:02 +1000

    Added: timer functionality to turret

--- Source/Tanks/Pawns/Turret.cpp
+++ Source/Tanks/Pawns/Turret.cpp
@@ -2,4 +2,27 @@
 
 
 #include "Tanks/Pawns/Turret.h"
+#include "Kismet/GameplayStatics.h"
 
+
+void ATurret::BeginPlay()
+{
+    Super::BeginPlay();
+
+    GetWorld()->GetTimerManager().SetTimer(FireRateTimerHandle, this, &ATurret::CheckFireCondition, FireRate, true);
+}
+
+// Called every frame
+void ATurret::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void ATurret::CheckFireCondition()
+{
+    // dont fire when no player
+
+    // fire when inrange
+
+    UE_LOG(LogTemp, Warning, TEXT("Fire Condition Checked"));
+}
