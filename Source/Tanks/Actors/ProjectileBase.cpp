3115e2ba8e2aecebe88b864b3e71ac0354b6eb89
DivadTenni <davidctenni@gmail.com>
2021-07-16 16:35:00 +1000

    Added: Projectile Uproperty definitions

--- Source/Tanks/Actors/ProjectileBase.cpp
+++ Source/Tanks/Actors/ProjectileBase.cpp
@@ -7,7 +7,7 @@
 AProjectileBase::AProjectileBase()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
@@ -16,12 +16,5 @@ void AProjectileBase::BeginPlay()
 {
 	Super::BeginPlay();
 	
-}
-
-// Called every frame
-void AProjectileBase::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
 }
 
--- Source/Tanks/Actors/ProjectileBase.h
+++ Source/Tanks/Actors/ProjectileBase.h
@@ -4,23 +4,40 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 #include "ProjectileBase.generated.h"
 
 UCLASS()
 class TANKS_API AProjectileBase : public AActor
 {
 	GENERATED_BODY()
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UProjectileMovementComponent* ProjectileMovement;
 	
+	UPROPERTY(EditDefaultsOnly, Category = "Damage")
+	UStaticMeshComponent* ProjectileMesh;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<UDamageType> DamageType;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float MovementSpeed = 1300;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Damage", meta = (AllowPrivateAccess = "true"))
+	float Damage = 50;
+
 public:	
 	// Sets default values for this actor's properties
 	AProjectileBase();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 };
