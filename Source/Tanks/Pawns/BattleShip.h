acf59bb9f2ab735f965fe21d82878221da0c709d
DivadTenni <davidctenni@gmail.com>
2021-08-01 20:54:48 +1000

    Added: to the .h of the battleship

--- Source/Tanks/Pawns/BattleShip.h
+++ Source/Tanks/Pawns/BattleShip.h
@@ -14,5 +14,25 @@ UCLASS()
 class TANKS_API ABattleShip : public AVehicleBase
 {
 	GENERATED_BODY()
-	
+private:
+	ATank* Player;
+	FTimerHandle FireRateTimerHandle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+		float FireRate = 2.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Combat", meta = (AllowPrivateAccess = "true"))
+		float Range = 600.f;
+
+
+	void CheckFireCondition();
+	float ReturnDistanceToPlayer();
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	virtual void HandleDestruction() override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 };
