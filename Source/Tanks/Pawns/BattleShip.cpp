46d96126e1a354550d725b8e303b5cbc14a4d106
DivadTenni <davidctenni@gmail.com>
2021-08-02 18:31:39 +1000

    Added: to battleship,.ccp

--- Source/Tanks/Pawns/BattleShip.cpp
+++ Source/Tanks/Pawns/BattleShip.cpp
@@ -3,3 +3,23 @@
 
 #include "BattleShip.h"
 
+void ABattleShip::CheckFireCondition()
+{
+}
+
+float ABattleShip::ReturnDistanceToPlayer()
+{
+	return 0.0f;
+}
+
+void ABattleShip::Tick(float DeltaTime)
+{
+}
+
+void ABattleShip::HandleDestruction()
+{
+}
+
+void ABattleShip::BeginPlay()
+{
+}
